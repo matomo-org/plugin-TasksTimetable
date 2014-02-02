2646ac6b7856fc46e20a0213a5467cd6cd7c7313
Fabian Becker <halfdan@xnorfz.de>
2014-02-03 00:16:20 +0100

    Removing @package/@subpackage/@category

--- Controller.php
+++ Controller.php
@@ -5,8 +5,6 @@
  * @link http://piwik.org
  * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
  *
- * @category Piwik_Plugins
- * @package TasksTimetable
  */
 namespace Piwik\Plugins\TasksTimetable;
 
@@ -18,7 +16,6 @@ use Piwik\Option;
 
 /**
  *
- * @package TasksTimetable
  */
 class Controller extends \Piwik\Plugin\ControllerAdmin
 {
--- TasksTimetable.php
+++ TasksTimetable.php
@@ -5,15 +5,12 @@
  * @link http://piwik.org
  * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
  *
- * @category Piwik_Plugins
- * @package TasksTimetable
  */
 namespace Piwik\Plugins\TasksTimetable;
 use Piwik\Piwik;
 use Piwik\Menu\MenuAdmin;
 
 /**
- * @package TasksTimetable
  */
 class TasksTimetable extends \Piwik\Plugin
 {

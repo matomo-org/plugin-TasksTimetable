79d30f4b1f22d090d26567ba84b824ae4173cf8c
sgiehl <stefan@piwik.org>
2016-10-31 22:41:37 +0100

    Replace usage of deprecated method Menu::add()

--- Menu.php
+++ Menu.php
@@ -15,12 +15,12 @@ class Menu extends \Piwik\Plugin\Menu
 {
     public function configureAdminMenu(MenuAdmin $menu)
     {
-        $menu->add(
-            'CoreAdminHome_MenuDiagnostic',
-            'TasksTimetable_ScheduledTasks',
-            array('module' => 'TasksTimetable', 'action' => 'index'),
-            $showOnlyIf = Piwik::hasUserSuperUserAccess(),
-            $order = 6
-        );
+        if (Piwik::hasUserSuperUserAccess()) {
+            $menu->addDiagnosticItem(
+                'TasksTimetable_ScheduledTasks',
+                array('module' => 'TasksTimetable', 'action' => 'index'),
+                $order = 6
+            );
+        }
     }
 }
